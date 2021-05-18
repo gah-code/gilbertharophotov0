f1ecadcfdfb533fb5776dbf8cdfcc8df3b5c82a4
Gilbert Haro <gilbertharocode@gmail.com>
2021-05-17 19:15:04 -0700

    First

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+import '../img/photo.jpg';
+import '../resources/images/engagement/*.jpg';
+import '../resources/images/family/*.jpg';
+import '../resources/images/wedding/*.jpg';
+import '../scss/main.scss';
