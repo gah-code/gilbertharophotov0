b1e7f0ff6efcb10286d6899793394d3eddce023a
Gilbert Haro <gilbertharocode@gmail.com>
2021-06-29 01:52:43 -0700

    June Late Night 28th

--- dist/main.fb6bbcaf.js
+++ dist/main.fb6bbcaf.js
@@ -22771,19 +22771,19 @@ allSections.forEach(function (section) {
 });
 },{"prettier":"../node_modules/prettier/standalone.js"}],"img/photo.jpg":[function(require,module,exports) {
 module.exports = "/photo.d379da31.jpg";
-},{}],"resources/images/branding/branding-1.jpg":[function(require,module,exports) {
-module.exports = "/branding-1.9131f212.jpg";
 },{}],"resources/images/branding/branding-2.jpg":[function(require,module,exports) {
 module.exports = "/branding-2.0e0dc949.jpg";
+},{}],"resources/images/branding/branding-1.jpg":[function(require,module,exports) {
+module.exports = "/branding-1.9131f212.jpg";
 },{}],"resources/images/branding/branding-3.jpg":[function(require,module,exports) {
 module.exports = "/branding-3.099fd3b8.jpg";
 },{}],"resources/images/branding/*.jpg":[function(require,module,exports) {
 module.exports = {
-  "branding-1": require("./branding-1.jpg"),
   "branding-2": require("./branding-2.jpg"),
+  "branding-1": require("./branding-1.jpg"),
   "branding-3": require("./branding-3.jpg")
 };
-},{"./branding-1.jpg":"resources/images/branding/branding-1.jpg","./branding-2.jpg":"resources/images/branding/branding-2.jpg","./branding-3.jpg":"resources/images/branding/branding-3.jpg"}],"resources/images/engagement/eng-1-large.jpg":[function(require,module,exports) {
+},{"./branding-2.jpg":"resources/images/branding/branding-2.jpg","./branding-1.jpg":"resources/images/branding/branding-1.jpg","./branding-3.jpg":"resources/images/branding/branding-3.jpg"}],"resources/images/engagement/eng-1-large.jpg":[function(require,module,exports) {
 module.exports = "/eng-1-large.6abe39dc.jpg";
 },{}],"resources/images/engagement/eng-1.jpg":[function(require,module,exports) {
 module.exports = "/eng-1.2db90b7f.jpg";
@@ -22800,23 +22800,23 @@ module.exports = {
 };
 },{"./eng-1-large.jpg":"resources/images/engagement/eng-1-large.jpg","./eng-1.jpg":"resources/images/engagement/eng-1.jpg","./eng-2.jpg":"resources/images/engagement/eng-2.jpg","./eng-3.jpg":"resources/images/engagement/eng-3.jpg"}],"resources/images/family/fam-1.jpg":[function(require,module,exports) {
 module.exports = "/fam-1.e5cf2e6b.jpg";
-},{}],"resources/images/family/fam-3-large.jpg":[function(require,module,exports) {
-module.exports = "/fam-3-large.8a57bbff.jpg";
 },{}],"resources/images/family/fam-3.jpg":[function(require,module,exports) {
 module.exports = "/fam-3.62fc2777.jpg";
+},{}],"resources/images/family/fam-3-large.jpg":[function(require,module,exports) {
+module.exports = "/fam-3-large.8a57bbff.jpg";
 },{}],"resources/images/family/fam-4.jpg":[function(require,module,exports) {
 module.exports = "/fam-4.1f68f66f.jpg";
 },{}],"resources/images/family/*.jpg":[function(require,module,exports) {
 module.exports = {
   "fam-1": require("./fam-1.jpg"),
-  "fam-3-large": require("./fam-3-large.jpg"),
   "fam-3": require("./fam-3.jpg"),
+  "fam-3-large": require("./fam-3-large.jpg"),
   "fam-4": require("./fam-4.jpg")
 };
-},{"./fam-1.jpg":"resources/images/family/fam-1.jpg","./fam-3-large.jpg":"resources/images/family/fam-3-large.jpg","./fam-3.jpg":"resources/images/family/fam-3.jpg","./fam-4.jpg":"resources/images/family/fam-4.jpg"}],"resources/images/wedding/wed-2-b.jpg":[function(require,module,exports) {
-module.exports = "/wed-2-b.574610a4.jpg";
-},{}],"resources/images/wedding/wed-1.jpg":[function(require,module,exports) {
+},{"./fam-1.jpg":"resources/images/family/fam-1.jpg","./fam-3.jpg":"resources/images/family/fam-3.jpg","./fam-3-large.jpg":"resources/images/family/fam-3-large.jpg","./fam-4.jpg":"resources/images/family/fam-4.jpg"}],"resources/images/wedding/wed-1.jpg":[function(require,module,exports) {
 module.exports = "/wed-1.fdd0a09c.jpg";
+},{}],"resources/images/wedding/wed-2-b.jpg":[function(require,module,exports) {
+module.exports = "/wed-2-b.574610a4.jpg";
 },{}],"resources/images/wedding/wed-2-large.jpg":[function(require,module,exports) {
 module.exports = "/wed-2-large.743ade08.jpg";
 },{}],"resources/images/wedding/wed-2.jpg":[function(require,module,exports) {
@@ -22827,14 +22827,14 @@ module.exports = "/wed-3.da704376.jpg";
 module.exports = "/wed-4.aaec434c.jpg";
 },{}],"resources/images/wedding/*.jpg":[function(require,module,exports) {
 module.exports = {
-  "wed-2-b": require("./wed-2-b.jpg"),
   "wed-1": require("./wed-1.jpg"),
+  "wed-2-b": require("./wed-2-b.jpg"),
   "wed-2-large": require("./wed-2-large.jpg"),
   "wed-2": require("./wed-2.jpg"),
   "wed-3": require("./wed-3.jpg"),
   "wed-4": require("./wed-4.jpg")
 };
-},{"./wed-2-b.jpg":"resources/images/wedding/wed-2-b.jpg","./wed-1.jpg":"resources/images/wedding/wed-1.jpg","./wed-2-large.jpg":"resources/images/wedding/wed-2-large.jpg","./wed-2.jpg":"resources/images/wedding/wed-2.jpg","./wed-3.jpg":"resources/images/wedding/wed-3.jpg","./wed-4.jpg":"resources/images/wedding/wed-4.jpg"}],"../node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
+},{"./wed-1.jpg":"resources/images/wedding/wed-1.jpg","./wed-2-b.jpg":"resources/images/wedding/wed-2-b.jpg","./wed-2-large.jpg":"resources/images/wedding/wed-2-large.jpg","./wed-2.jpg":"resources/images/wedding/wed-2.jpg","./wed-3.jpg":"resources/images/wedding/wed-3.jpg","./wed-4.jpg":"resources/images/wedding/wed-4.jpg"}],"../node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
 var bundleURL = null;
 
 function getBundleURLCached() {
@@ -22950,7 +22950,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54177" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61845" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
